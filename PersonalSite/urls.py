0dd7c74a4eb614f0f5ebd25e03470de26ce87a33
Jake <jpierrepont16@gmail.com>
2020-08-17 10:00:44 +0000

    added dummy home page

--- PersonalSite/urls.py
+++ PersonalSite/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.conf.urls import url, include
+from home import views
 
 urlpatterns = [
     url('admin/', admin.site.urls),
-    url('', include('blog.urls')),
+    url(r'^$', views.home_page, name='home'),
 ]
